77111ab9e8f92e18c7a6ee6ab22973403d915315
svonidze <svonidze@1cf73f64-d134-4b96-b1c8-b4bf8e25d8aa>
2013-05-08 19:37:39 +0000

    fix: - создан общий проект типов - DI выделены интерфейсы таймеров

---
+++ SBeep.Private.Useful.Cuckoo.Common/Sources/Pub/Interfaces/Timers/IManualTimer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SBeep.Private.Useful.Cuckoo.Common.Pub.Interfaces.Timers
+{
+    public interface IManualTimer {
+        TimeSpan NextCallBackTime { get; }
+        DateTime? StartTime { get; }
+        void SetPeriod( TimeSpan period );
+        void Start( TimeSpan? dueTime = null );
+        void Pause();
+    }
+}
